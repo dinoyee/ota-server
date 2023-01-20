57506855603fec03f41a1d358b28a89d2ec77671
dinoyee <yeederan1991@gmail.com>
2023-01-21 05:54:42 +0800

    feat(): del ota-vue

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -10,7 +9,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'ota-vue/dist')));
+
 app.use('/', router);
 
 export default app;
