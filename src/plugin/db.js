87cecbb2c2e984fcad1fa471703d2caaea6d36f1
dinoyee <dino@iter.com.tw>
2023-02-01 23:23:56 +0800

    feat: db setting default admin user

--- src/plugin/db.js
+++ src/plugin/db.js
@@ -1,13 +1,30 @@
 import mongoose from 'mongoose';
+import { findAdmin, createAdmin } from '../repository/user';
 
 const mongoDB = Env.MONGO_URL;
 mongoose.set('strictQuery', false);
-mongoose.connect(mongoDB, {
+
+const dbOptions = {
   user: Env.MONGO_USER,
   pass: Env.MONGO_PASS,
-}).then(() => {
-  LogI('Connected to MongoDB');
-});
+};
+
+if (Env.MONGO_USER === undefined || Env.MONGO_PASS === undefined) {
+  delete dbOptions.user;
+  delete dbOptions.pass;
+}
+
+mongoose.connect(mongoDB, dbOptions)
+  .then(async () => {
+    LogI('Connected to MongoDB');
+    const admin = await findAdmin();
+    if (!admin) {
+      const user = await createAdmin();
+      LogI({ msg: 'Create Admin', user });
+    } else {
+      LogI('Has Admin');
+    }
+  });
 
 mongoose.connection.on('error', () => {
   LogE('MongoDB connection error');
--- src/repository/user.js
+++ src/repository/user.js
@@ -1,5 +1,10 @@
 import UserSchema from '../models/user';
 
+// Admin
+export const findAdmin = async () => UserSchema.findOne({ username: 'admin' });
+
+export const createAdmin = async () => UserSchema.create({ username: 'admin', password: 'admin', isAdmin: true });
+
 export const doLogin = async (username, password) => UserSchema.findOne({ username, password }).lean();
 
 export const getUserInfo = async (_id) => UserSchema.findById(_id, '-_id username isAdmin').exec();
